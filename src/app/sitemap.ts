a9ac12833c624f174c9dc1e576851b006a3e4430
Ninni Puglisi <arch.ninni@gmail.com>
2026-06-06 20:50:21 +0200

    Add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://studionp39.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://studionp39.com/studio",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://studionp39.com/projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://studionp39.com/projects/villa-de",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://studionp39.com/projects/max-boutique",
+      lastModified: new Date(),
+    },
+  ];
+}
